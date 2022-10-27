83739c3e88a7a1cb75fb20967ba200d21d9c3d48
GGalli <115507314+userG3336393@users.noreply.github.com>
2022-10-27 15:15:04 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -51,7 +51,7 @@ particlesJS('particles-js',
         }
       },
       "size": {
-        "value": 5,
+        "value": 3,
         "random": true,
         "anim": {
           "enable": false,
@@ -62,7 +62,7 @@ particlesJS('particles-js',
       },
       "line_linked": {
         "enable": true,
-        "distance": 150,
+        "distance": 100,
         "color": "#ffffff",
         "opacity": 0.4,
         "width": 1
@@ -74,6 +74,7 @@ particlesJS('particles-js',
         "random": false,
         "straight": false,
         "out_mode": "out",
+        "bounce" : false,
         "attract": {
           "enable": false,
           "rotateX": 600,
@@ -86,36 +87,36 @@ particlesJS('particles-js',
       "events": {
         "onhover": {
           "enable": true,
-          "mode": "repulse"
+          "mode": "grab"
         },
         "onclick": {
           "enable": true,
-          "mode": "push"
+          "mode": "repulse"
         },
         "resize": true
       },
       "modes": {
         "grab": {
-          "distance": 400,
+          "distance": 270,
           "line_linked": {
             "opacity": 1
           }
         },
         "bubble": {
-          "distance": 400,
+          "distance": 240,
           "size": 40,
           "duration": 2,
           "opacity": 8,
-          "speed": 3
+          "speed": 2
         },
         "repulse": {
           "distance": 200
         },
         "push": {
-          "particles_nb": 4
+          "particles_nb": 8
         },
         "remove": {
-          "particles_nb": 2
+          "particles_nb": 6
         }
       }
     },
@@ -131,3 +132,12 @@ particlesJS('particles-js',
   }
 
 );
+
+var count_particles, stats, update; stats = new Stats; stats.setMode(0); 
+stats.domElement.style.position = 'absolute'; stats.domElement.style.left = '0px'; 
+stats.domElement.style.top = '0px'; document.body.appendChild(stats.domElement); 
+count_particles = document.querySelector('.js-count-particles'); 
+update = function() { stats.begin(); stats.end(); 
+  if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) 
+    { count_particles.innerText = window.pJSDom[0].pJS.particles.array.length; } 
+  requestAnimationFrame(update); }; requestAnimationFrame(update);;
